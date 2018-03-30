eb18641b4408f7677dd13cb59ceb98b9a73be8b3
REED BLACK <reedblack@REEDs-MacBook-Air.local>
2018-03-30 12:52:42 -0600

    committing changes

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const baseUrl = 'https://www.reddit.com/r/cats/top/.json?count=25'
-const page2Url = 'https://www.reddit.com/r/cats/top/.json?count=25&after=t3_881wyt'
-const page3Url = 'https://www.reddit.com/r/cats/top/.json?count=50&after=t3_87xa78'
-const page4Url = 'https://www.reddit.com/r/cats/top/.json?count=75&after=t3_882nn5'
+const page2Url = 'https://www.reddit.com/r/cats/top/.json?count=25&after'
+const page3Url = 'https://www.reddit.com/r/cats/top/.json?count=50&after'
+const page4Url = 'https://www.reddit.com/r/cats/top/.json?count=75&after'
 const catDisplay = document.querySelector('.cats')
+const urlArr = []
 
 catDisplay.addEventListener('click', showCats)
 
@@ -14,11 +15,10 @@ function showCats() {
       return response.json()
     })
     .then(function(data) {
-      var urlArr = []
       for (var i = 0; i < data.data.children.length; i++) {
         let currentPost = data.data.children[i]
-        if (!currentPost.data.url.includes('.jpg')) {} else {
-          urlArr.push(currentPost.data.url);
+        if (!currentPost.data.url.includes('jpg')) {} else {
+          urlArr.push(currentPost.data.url)
         }
       }
       fetch(page2Url)
@@ -26,39 +26,38 @@ function showCats() {
           return response.json()
         })
         .then(function(moreCats) {
-          var fullerUrl = urlArr
           for (var j = 0; j < moreCats.data.children.length; j++) {
             let currentPost2 = moreCats.data.children[j]
-            if (!currentPost2.data.url.includes('.jpg')) {} else {
-              fullerUrl.push(currentPost2.data.url);
-              console.log(urlArr);
+            if (!currentPost2.data.url.includes('.jpg')) {
+            } else {
+              urlArr.push(currentPost2.data.url)
             }
           }
-          fetch(page4Url)
+          fetch(page3Url)
             .then(function(response) {
               return response.json()
             })
             .then(function(moar) {
-              var moreFullerUrl = fullerUrl
-              for (var j = 0; j < moar.data.children.length; j++) {
-                let currentPost4 = moar.data.children[j]
-                if (!currentPost4.data.url.includes('.jpg')) {} else {
-                  moreFullerUrl.push(currentPost4.data.url);
+              for (var k = 0; k < moar.data.children.length; k++) {
+                let currentPost3 = moar.data.children[k]
+                if (!currentPost3.data.url.includes('.jpg')) {
+                } else {
+                  urlArr.push(currentPost3.data.url)
                 }
               }
-              fetch(page3Url)
+              fetch(page4Url)
                 .then(function(response) {
                   return response.json()
                 })
                 .then(function(evenMoreCats) {
-                  var fullestUrl = moreFullerUrl
-                  for (var j = 0; j < evenMoreCats.data.children.length; j++) {
-                    let currentPost3 = evenMoreCats.data.children[j]
-                    if (!currentPost3.data.url.includes('.jpg')) {} else {
-                      fullestUrl.push(currentPost3.data.url);
+                  for (var l = 0; l < evenMoreCats.data.children.length; l++) {
+                    let currentPost4 = evenMoreCats.data.children[l]
+                    if (!currentPost4.data.url.includes('.jpg')) {
+                    } else {
+                      urlArr.push(currentPost4.data.url)
                     }
                   }
-                  var randomUrl = fullestUrl[Math.floor(Math.random() * fullestUrl.length)]
+                  var randomUrl = urlArr[Math.floor(Math.random() * urlArr.length)]
                   catDisplay.src = randomUrl
                 })
             })
